ae0039d33c4b67f0d3f38538f5374e712f78c970
Nika <srkveronica@gmail.com>
2024-10-27 21:42:38 +0300

    Register admin

--- blog/catalog/models.py
+++ blog/catalog/models.py
@@ -6,3 +6,14 @@ class Article(models.Model):
     summary = models.TextField(verbose_name='summary')
     date = models.DateField(verbose_name='date of publishing')
     text = models.TextField(verbose_name='text')
+    image = models.ImageField(blank=True,
+                              upload_to='img',
+                              verbose_name='image')
+
+    class Meta:
+        verbose_name = 'article'
+        verbose_name_plural = 'articles'
+    
+    def __str__(self):
+        title = self.title
+        return title
--- blog/catalog/views.py
+++ blog/catalog/views.py
@@ -1,18 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
 def catalog(request):
     template = 'catalog/catalog.html'
-    catalog = Article.objects.all()
+    catalog = Article.objects.all().order_by('date')
 
     context = {'catalog': catalog}
     return render(request, template, context)
 
 
-def article(request):
-    template = 'catalog/catalog.html'
-    article = Article.objects.all()
-
+def article(request, pk):
+    template = 'catalog/article.html'
+    article = get_object_or_404(Article, pk=pk)
     context = {'article': article}
     return render(request, template, context)
